e7d8e15cc404c51368dea2ce1cb2e709d1ee8a51
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2024-03-11 00:31:00 +0200

    add react routing between pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import NavigationBar from "./components/Navbar/Navbar";
-// import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import AppLayout from "../src/pages/AppLayout.jsx";
+import Jobs from "./pages/Jobs/Jobs.jsx";
+import Companies from "./pages/Companies/Companies.jsx";
+import Homepage from "./pages/Homepage/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
 
 export default function App() {
   return (
-    <div>
-      <NavigationBar />
-      {/* <Footer /> */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="companies" element={<Companies />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
