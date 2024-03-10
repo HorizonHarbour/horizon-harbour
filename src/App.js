e7e309a0a091e17584400566fc1e7948fe035971
Ahmed Ashraf <ahmed.ashraf.saad@gmail.com>
2024-03-10 22:31:17 +0200

    feat(footer) finish footer design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavigationBar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
 
 export default function App() {
   return (
     <div>
       <NavigationBar />
+      {/* <Footer /> */}
     </div>
   );
 }
